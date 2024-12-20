6927e6593742d2bdea1c963fea72391d98773370
hzinchenko <hzinchenko@ecisys.com>
2024-12-20 17:41:23 +0200

    initial

---
+++ app/screens/Home.styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
+});
+
+export default styles;
