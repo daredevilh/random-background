119390b0fd495d4d33de78a1ee89396e2667c46f
hzinchenko <hzinchenko@ecisys.com>
2024-12-20 18:09:40 +0200

    added random color generator

--- app/screens/Home.styles.ts
+++ app/screens/Home.styles.ts
@@ -1,12 +1,16 @@
 import {StyleSheet} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     fontSize: 25,
     fontWeight: '500',
+    color: Colors.black,
   },
 });
 
